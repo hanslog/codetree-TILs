98b7e723830a00b730d4ecf0791270b385f312a7
hanslog <111857426+hanslog@users.noreply.github.com>
2023-11-10 18:14:32 +0900

    Title: 약수의 개수가 3개인 수, Time: 118ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 231110/약수의 개수가 3개인 수/square-of-prime-number.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int start=sc.nextInt();
+        int end=sc.nextInt();
+        int cnt=0;
+        for(int i=1;i<=start;i++){
+            for(int j=1;j<=end;j++){
+                if(j%4==0)
+                    cnt++;
+                if(cnt==3)
+                    cnt=1;
+            }
+        }
+        System.out.println(cnt);
+    }
+}
