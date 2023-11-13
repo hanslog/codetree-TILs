01b23f8f29f7a80676c44b72dd2c47132de590d7
hanslog <111857426+hanslog@users.noreply.github.com>
2023-11-13 21:20:45 +0900

    Title: 배열의 특정 위치 찾기 4, Time: 138ms, Memory: 10MB, Status: Passed - CodeTree

--- 231113/배열의 특정 위치 찾기 4/find-specific-location-of-array-4.java
+++ 231113/배열의 특정 위치 찾기 4/find-specific-location-of-array-4.java
@@ -2,15 +2,29 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
+        //입력받을내용 문자로 바꾸기 
+        String[] input=sc.nextLine().split(" ");
+        int[] arr=new int[input.length];
+        for(int i=0;i<input.length;i++){
+            arr[i]=Integer.parseInt(input[i]);
+        }
         int cnt=0;
         int sum=0;
-        for(int i=0;i<10;i++){
-            int n=sc.nextInt();
-            if(n%2==0){
-                cnt++;
-                sum+=n;
-            }
+        int cnt2=0;
+        //입력받을내용숫자세기위해 for each로 써주기
+    for(int array: arr){
+        if(array==0){
+            break;
         }
-        System.out.println(String.format("%d %d",cnt-1,sum));
+        cnt++;
+    }
+    for(int i=0;i<cnt;i++){
+        if(arr[i]%2==0){
+            sum+=arr[i];
+            //주의사항 2의배수에 1은 제외하므로 cnt2는 1더해줘서 2부터시작
+            cnt2+=1;
+        }
+    }
+        System.out.println(cnt2+" "+sum);
     }
 }
