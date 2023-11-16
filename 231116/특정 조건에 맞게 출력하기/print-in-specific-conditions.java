b6e198308032c63e917965169201a8de08510f7b
hanslog <111857426+hanslog@users.noreply.github.com>
2023-11-16 20:24:44 +0900

    Title: 특정 조건에 맞게 출력하기, Time: 136ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 231116/특정 조건에 맞게 출력하기/print-in-specific-conditions.java
@@ -0,0 +1,33 @@
+import java.util.*;
+public class Main {
+
+  //0이주어지면 0제외 /주어진정수차례로출력/홀수이면 +3/짝수 /2= 몫출력  
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+        int[] arr=new int[100];
+        int[] arr2=new int[100];
+
+        int point=0;
+
+        int i=0;
+        //Scanner .hasNext() boolean조건형 함수 여기서 있으면 true, false로 사용
+        while(sc.hasNext()){
+            arr[i]=sc.nextInt();
+            if(arr[i]==0)
+                point=i;
+
+            if(arr[i]%2==0)
+                arr2[i]=arr[i]/2;
+            else
+                arr2[i]=arr[i]+3;
+            
+            i++;
+        }
+
+        //출력
+        for(int k=0;k<point;k++){
+            System.out.print(arr2[k]+" ");
+        }
+    }
+}
