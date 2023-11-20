b27700131ef6ee65fbe246cd2aa028cfc47ad4d3
hanslog <111857426+hanslog@users.noreply.github.com>
2023-11-20 19:39:51 +0900

    Title: 가장 큰 수 고르기, Time: 152ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 231120/가장 큰 수 고르기/picking-biggest-number.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int[] arr=new int[10];
+        int max=0;
+        for(int i=0;i<10;i++){
+            arr[i]=sc.nextInt();
+            if(arr[i]>max)
+            max=arr[i];
+        }
+
+        System.out.println(max);
+        
+    }
+}
