64605e5cf47ad0486dbba33b827ec903472ad7a0
hanslog <111857426+hanslog@users.noreply.github.com>
2023-11-21 17:12:12 +0900

    Title: 공백을 기준으로 출력, Time: 114ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 231121/공백을 기준으로 출력/output-based-on-space.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Main {
+    //공백제거하기 outcome="";붙이기!!
+    public static void main(String[] args) {
+       Scanner input=new Scanner(System.in);
+       String str=input.nextLine();
+       String str2=input.nextLine();
+
+       System.out.print(str.replaceAll(" ", ""));
+          System.out.print(str2.replaceAll(" ", ""));
+    }
+}
