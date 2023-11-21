526d60ef040cf12093b243b28cd6095451016424
hanslog <111857426+hanslog@users.noreply.github.com>
2023-11-21 18:54:28 +0900

    Title: 대문자 소문자 바꾸기, Time: 124ms, Memory: 10MB, Status: Passed - CodeTree

--- 231121/대문자 소문자 바꾸기/change-uppercase-and-lowercase.java
+++ 231121/대문자 소문자 바꾸기/change-uppercase-and-lowercase.java
@@ -1,21 +1,24 @@
 import java.util.*;
 //대문자는 소문자로 소문자는 대문자로
-//Character.isUpperCase(arr[i].charAt(0))먼저 걸으면서 바꿔주기 동시에 반대로 바껴야하니까
-// int는 index는 돌뿐이다 String length가지고 비교해도 된다 for문 char로 바꿔주지않아도 비교돼 헷갈리지말것
+//오랜만이야 .toCharArray(); 이번엔 뿌리뽑자~!!바꾸자~!! Stirng 에서 char[]바꾸는는데 toCharArray()사용 :문자열을 한글자씩 쪼개서 배열에 집어넣어준다
+//  중요*** 문자가 대문자인지 먼저 물어봐준다 !! if(Character.isUpperCase(arr[i])) 
+//String.valueOf() 객체를 문자열변환!! 너아주 대소문자 내손에 죽는다! 아주 그냥!! 
 public class Main {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        String input=sc.nextLine();
-                
-        String result=input.replaceAll("CodeTree","cODEtREE");
-    
-        // int length=input.length();
-        // input=input.toUpperCase;
-        // for(int i=0;i<length;i++){
-        //     if(Character.isLetter(input.charAt(0) && input.charAt(4)))
-        //         input=input.toLowerCase;
-        // }
-       System.out.println(result);
+        String input=sc.next();
+        char[] arr=input.toCharArray();        
+        for(int i=0;i<arr.length;i++){
+
+            if(Character.isUpperCase(arr[i])){
+                arr[i]=Character.toLowerCase(arr[i]);
+            }
+            else{
+                arr[i]=Character.toUpperCase(arr[i]);
+            }
+        }
+        input=String.valueOf(arr);
+       System.out.println(input);
 
 
     }
