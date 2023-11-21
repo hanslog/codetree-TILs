c2191579cc05ede2166870ad578167b9d4a88063
hanslog <111857426+hanslog@users.noreply.github.com>
2023-11-21 16:57:14 +0900

    Title: 문자 개수 세기, Time: 131ms, Memory: 10MB, Status: Passed - CodeTree

--- 231121/문자 개수 세기/count-char.java
+++ 231121/문자 개수 세기/count-char.java
@@ -3,22 +3,15 @@ public class Main {
     //소문자 알파벳이 주어지면 앞서 주어진 문자열에 소문자 알파벳이 몇개인지 출력
     public static void main(String[] args) {
             Scanner alpha=new Scanner(System.in);
-            String insert=alpha.next();
-          
-    System.out.println(countChar(insert,'e'));
-
-
-
-    }
-
-      public static int countChar(String alpha, char ch){
-        int count=1;
-
-        for(int i=0;i<alpha.length();i++){
-            if(alpha.charAt(i)==ch){
-                count++;
+            String insert=alpha.nextLine();
+          //e와 a 하나씩 맞춰야 하니까 .next().charAt(0);
+            char a=alpha.next().charAt(0);
+            int cnt=0;
+            for(int i=0;i<insert.length();i++){
+                if(insert.charAt(i)==a){
+                    cnt++;
+                }
             }
-        }
-        return count;
+            System.out.println(cnt);
     }
 }
