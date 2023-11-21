1600634c61dc66cea769c5b7132bf9c619458258
hanslog <111857426+hanslog@users.noreply.github.com>
2023-11-21 16:17:56 +0900

    Title: 문자열 범위 출력하기, Time: 122ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 231121/문자열 범위 출력하기/print-string-in-range.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+        String str=sc.nextLine();
+        
+        for(int i=2;i<str.length()-3;i++){
+            //위치 지점을 나타내기 때문에 .charAt();쓰는게 좋음
+            System.out.print(str.charAt(i));
+        }
+     
+    }
+}
