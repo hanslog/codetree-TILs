d8a92bc5805d08cf5f8d6b7aac12583e1f1fd2f2
hanslog <111857426+hanslog@users.noreply.github.com>
2023-11-24 20:48:35 +0900

    Title: 숫자로 이루어진 사각형, Time: 196ms, Memory: 12MB, Status: Passed - CodeTree

--- 231124/숫자로 이루어진 사각형/rectangle-with-a-number.java
+++ 231124/숫자로 이루어진 사각형/rectangle-with-a-number.java
@@ -1,11 +1,12 @@
 import java.util.*;
 //정수 N값 주어지면 N*N모양 사각형 함수전달받아 출력
+//n을 4로 해줬다구 틀리냐 참나.....//N 입력값있어서 매개변수 n있어야 되고 n값으로 조건값줘야됨 4로 하면안된다
 public class Main {
-    public static void numberFour(){
+    public static void numberFour(int n){
          int cnt=1;
-        for(int i=1;i<=4;i++){
+        for(int i=1;i<=n;i++){
            
-            for(int j=1;j<=4;j++){
+            for(int j=1;j<=n;j++){
                 System.out.print(cnt+" ");
                 cnt++;
                 if(cnt==10)
@@ -19,7 +20,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int N=sc.nextInt();
-        numberFour();
+        numberFour(N);
             
     
     }
