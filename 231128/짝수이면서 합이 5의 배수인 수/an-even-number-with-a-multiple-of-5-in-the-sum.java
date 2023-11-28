cfd64582a7847a4910dff71cf8020a9ea6092135
hanslog <111857426+hanslog@users.noreply.github.com>
2023-11-28 17:45:47 +0900

    Title: 짝수이면서 합이 5의 배수인 수, Time: 126ms, Memory: 10MB, Status: Passed - CodeTree

--- 231128/짝수이면서 합이 5의 배수인 수/an-even-number-with-a-multiple-of-5-in-the-sum.java
+++ 231128/짝수이면서 합이 5의 배수인 수/an-even-number-with-a-multiple-of-5-in-the-sum.java
@@ -2,16 +2,24 @@ import java.util.*;
 public class Main {
     //boolean 으로 함수만들기-여러가지가있다 그중하나
     //2자리 숫자 n이 주어져 n이 짝수이면서 각자리 숫자합이 5의배수 Yes /아니라면 No출력
-    public static void doublefive(int n){
-        if((n%2==0) && ((n/10)+(n%10))%5==0)
-            System.out.println("Yes");
-        else
-            System.out.println("No");
-    }
+    // public static void doublefive(int n){
+    //     if((n%2==0) && ((n/10)+(n%10))%5==0)
+    //         System.out.println("Yes");
+    //     else
+    //         System.out.println("No");
+    // }
+        public static boolean doublefive(int n){
+            return n%2==0 && (n/10+(n%10)) % 5==0;
+        }
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
+      //  doublefive(n);
 
-        doublefive(n);
+        //숫자합이 맞으면 true면 호출받기
+        if(doublefive(n))
+            System.out.println("Yes");
+        else
+            System.out.println("No");
     }
 }
