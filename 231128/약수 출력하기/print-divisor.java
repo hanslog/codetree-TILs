c5ed3b15ccde048017609f18275c2de2127f32a4
hanslog <111857426+hanslog@users.noreply.github.com>
2023-11-28 21:41:34 +0900

    Title: 약수 출력하기, Time: 3004ms, Memory: 9MB, Status: Time Limit Exceed - CodeTree

--- 231128/약수 출력하기/print-divisor.java
+++ 231128/약수 출력하기/print-divisor.java
@@ -3,14 +3,11 @@ public class Main {
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
         double N=sc.nextInt();
-        try{
+        
         for(int i=0;i<=N;i++){
             if((double)N%i==0)
             System.out.print(i+" ");
         }
-        }
-        catch(Exception e){
-            System.out.print(e);
-        }
+    
     }
 }
