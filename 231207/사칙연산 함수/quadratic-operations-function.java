43132d7219ac9bfe18ed94ee54439e2fc5e3f864
hanslog <111857426+hanslog@users.noreply.github.com>
2023-12-07 16:30:25 +0900

    Title: 사칙연산 함수, Time: 128ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 231207/사칙연산 함수/quadratic-operations-function.java
@@ -0,0 +1,31 @@
+//작업중!! 배운거 부분적으로 활용해보기
+//나중은 전체적으로
+import java.util.*;
+public class Main {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        char sline=sc.next().charAt(0);
+        int c=sc.nextInt();
+ 
+        System.out.println(a+" "+sline+" "+c+" = "+strToNumber(a,c));
+       
+    }
+    public static int strToNumber(int a, int c){
+        String[] sline={"/*+-"};
+               int number=0;
+
+        for(int i=0;i<=sline.length-1;i++){
+            if(sline[i].contains("*"))
+                number=a*c;
+            else if(sline[i].contains("/"))
+                number=a/c;
+            else if(sline[i].contains("+"))
+                number=a+c;
+            else if(sline[i].contains("-"))
+                number=a-c;
+        }
+        return number;
+    }
+  
+}
