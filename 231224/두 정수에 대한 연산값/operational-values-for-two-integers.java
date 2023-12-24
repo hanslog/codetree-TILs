0a64e7b68b61d8b3a6a6ca404b8095fd52e49c96
hanslog <111857426+hanslog@users.noreply.github.com>
2023-12-24 20:16:48 +0900

    Title: 두 정수에 대한 연산값, Time: 133ms, Memory: 10MB, Status: Passed - CodeTree

--- 231224/두 정수에 대한 연산값/operational-values-for-two-integers.java
+++ 231224/두 정수에 대한 연산값/operational-values-for-two-integers.java
@@ -5,14 +5,32 @@ public class Main {
         Scanner sc=new Scanner(System.in);
         int a=sc.nextInt();
         int b=sc.nextInt();
-        plusdouble(a,b);
+        IntNumber aNumber=new IntNumber(a);
+        IntNumber bNumber=new IntNumber(b);
+
+        plusdouble(aNumber,bNumber);
+
+        a=aNumber.number;
+        b=bNumber.number;
+
+        System.out.print(a+" "+b);
     }
-    public static void plusdouble(int a,int b){
-        if(a<b){
-            System.out.print(a*2+" ");
+    public static void plusdouble(IntNumber a,IntNumber b){
+        if(a.number>b.number){
+           b.number*=2;
+           a.number+=25;
+        }else{
+           a.number*=2;
+           b.number+=25;
         }
-        if(b>a){
-            System.out.print(b+25+" ");
-        }
+  //     return;
+    }
+}
+
+class IntNumber{
+    int number;
+
+    public IntNumber(int number){
+        this.number=number;
     }
 }
